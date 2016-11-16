9814fdfd9d950b717337620963f8c45bcb9dd4eb
CdrXander <xackhackman@gmail.com>
2016-11-16 12:38:35 -0700

    Pagination included

--- controller.js
+++ controller.js
@@ -1,10 +1,26 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
 
-  $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
-  }
+	$scope.currentPage = 1;
 
+	$scope.getUsers = function() {
+	  	mainService.getUsers($scope.currentPage).then(function(response) {
+	  		$scope.users = response.data.data;
+	  		$scope.totalPages = response.data.total_pages;
+	 	});
+	}
+	$scope.getUsers();
 
-  $scope.getUsers();
+	$scope.prev = function() {
+		if($scope.currentPage > 1) {
+			$scope.currentPage--;
+			$scope.getUsers();
+		}
+	}
 
+	$scope.next = function() {
+		if($scope.currentPage < $scope.totalPages) {
+			$scope.currentPage++;
+			$scope.getUsers();
+		}
+	}
 });
